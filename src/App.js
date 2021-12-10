4de6c14bbddc8455c48b387037e06db042c65652
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2021-12-10 16:40:48 -0400

    reset project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, {createContext, useState} from 'react';
 
-import Counter from "./Counter";
-
-export const ThemeContext = createContext();
-
 
 function App() {
   const [theme, setTheme] = useState('blue')
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      <Counter initialCount={0} />
-      <button onClick={
-        () => setTheme(prevTheme => {
-          return prevTheme === 'blue' ? 'green' : 'blue';
-        })
-      }>Toggle theme</button>
-    </ThemeContext.Provider>
+    null
   );
 }
 
