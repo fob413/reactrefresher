19cfd19df2d8734fbd43330be3e4eeabf2262455
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2021-12-10 16:34:30 -0400

    updated repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useState} from 'react';
+
+import Counter from "./Counter";
+
+export const ThemeContext = createContext();
+
 
 function App() {
+  const [theme, setTheme] = useState('blue')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      <Counter initialCount={0} />
+      <button onClick={
+        () => setTheme(prevTheme => {
+          return prevTheme === 'blue' ? 'green' : 'blue';
+        })
+      }>Toggle theme</button>
+    </ThemeContext.Provider>
   );
 }
 
